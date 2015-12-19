bfba861e6ed096612f9ff176588dc1b2db3655a9
Kavyahs <kshanthegowda@qwinixtech.com>
2015-12-19 17:38:23 +0530

    category and sub categories added

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219090729) do
+ActiveRecord::Schema.define(version: 20151219115943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "districts", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +33,13 @@ ActiveRecord::Schema.define(version: 20151219090729) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sub_categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
