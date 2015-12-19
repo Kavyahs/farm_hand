12b2b1b8ea9708b2b522f8a607b741630b40cd92
asimha <asimha@qwinixtech.com>
2015-12-19 23:12:06 +0530

    product page made dynamic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    @products = Product.where('category_id = ?', params[:category_id])
   end
 
   def new
