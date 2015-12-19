3987546f994c087abd10e2ff0a436376106647db
vikrammirle <vmchandrashekar@qwinixtech.com>
2015-12-19 22:39:24 +0530

    My profile HTML page

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Rails.application.routes.draw do
   get 'static/products' => 'statics#products'
   get 'static/bidding' => 'statics#bidding'
   get 'static/my_activity' => 'statics#my_activity'
+  get 'static/my_profile' => 'statics#my_profile'
 
   # Example resource route with sub-resources:
   #   resources :products do
