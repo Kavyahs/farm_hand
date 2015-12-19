bed83d5c5f1506944a28e166cffb589df524f7d7
Prakash S L <plingaiah@qwinixtech.com>
2015-12-19 17:09:41 +0530

    added validation and also its specs.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ActiveRecord::Base
 
   validates :images, presence: true
-  validates :category, presence: true
+  validates :sub_category_id, presence: true
   validates :phone, presence: true
   validates :address, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
-  validates :state, presence: true
-  validates :district, presence: true
+  validates :state_id, presence: true
+  validates :district_id, presence: true
   validates :close_date, presence: true
   validates :seller_name, presence: true
   validates :email, presence: true
